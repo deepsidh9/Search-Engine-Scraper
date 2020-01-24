d9df26f5ce342b0290da0af2680743dd46ff04ae
deepsidh9 <deep.barca@gmail.com>
2020-01-25 00:31:49 +0530

    Fixed User agent bug

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='search_engine_scraper',
-      version='0.1',
+      version='0.3',
       description='A module to scrape popular search engines',
       long_description=long_description,
        long_description_content_type='text/markdown',
