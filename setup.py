f2431c1744239c4f5d6492ba0ac7ee917b762abe
deepsidh9 <deep.barca@gmail.com>
2020-01-24 22:33:06 +0530

    updated setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='A module to scrape popular search engines',
+      url='https://github.com/deepsidh9/Search-Engine-Scraper',
+      author='Deep Singh Dhillon',
+      author_email='deep.barca@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
